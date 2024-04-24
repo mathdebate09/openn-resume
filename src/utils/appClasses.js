b76ebdd5efb507af6a80d4c89337e238bf9983fa
Jay Singh <learn.jayssingh@gmail.com>
2024-04-24 20:43:13 +0530

    Add heading component, icons and utility classes for Education, Activity, and Skills

---
+++ src/utils/appClasses.js
@@ -0,0 +1,30 @@
+class Education {
+    constructor() {
+        this.startDate = '';
+        this.endDate = '';
+        this.course = '';
+        this.grade = '';
+        this.id = '';
+    }
+}
+
+// Class to be used to Work/Project section
+class Activity {
+    constructor() {
+        this.designation = '';
+        this.startDate = '';
+        this.endDate = '';
+        this.description = '';
+        this.id = '';
+    }
+}
+
+class Skills {
+    constructor() {
+        this.position = '';
+        this.techstack = '';
+        this.id = '';
+    }
+}
+
+export {Education, Activity, Skills, };
